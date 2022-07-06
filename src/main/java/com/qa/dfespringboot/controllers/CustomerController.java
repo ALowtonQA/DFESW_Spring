c5ba9473b4d371a1e09e7bee45631c8649556f10
ALowton <ALowton@QA.com>
2022-07-06 12:14:33 +0100

    AL: controller basic CRUD

--- src/main/java/com/qa/dfespringboot/controllers/CustomerController.java
+++ src/main/java/com/qa/dfespringboot/controllers/CustomerController.java
@@ -3,8 +3,11 @@ package com.qa.dfespringboot.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,10 +24,18 @@ public class CustomerController {
 	private List<Customer> customers = new ArrayList<>();
 	
 	//GET - READ
-//	@GetMapping("/hello") //localhost:8080/customer/hello
-//	public String hello() {
-//		return "Hello";
-//	}
+	//ReadAll
+	@GetMapping("/readAll")
+	public List<Customer> readAll() {
+		return this.customers;
+	}
+	
+	//ReadByID
+	@GetMapping("/readById/{id}")
+	public Customer readById(@PathVariable int id) {
+		return this.customers.get(id);
+	}
+	
 	
 	//POST - CREATE
 	@PostMapping("/create") // localhost:8080/customer/create
@@ -36,8 +47,21 @@ public class CustomerController {
 	}
 	
 	//PUT - UPDATE
-	
+	@PutMapping("/update/{id}")
+	public Customer update(@PathVariable int id, @RequestBody Customer customer) {
+		// Removing the original customer
+		this.customers.remove(id);
+		
+		// Add the updated customer
+		this.customers.add(id, customer);
+		
+		// Return the updated user
+		return this.customers.get(id);
+	}
 	
 	//DELETE - DELETE
-	
+	@DeleteMapping("/delete/{id}")
+	public Customer delete(@PathVariable int id) {
+		return this.customers.remove(id);
+	}
 }
